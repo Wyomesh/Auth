77ec5ce9dceb74361b3ffdbdce5e2f75eb13cf99
Wyomesh <wyomeshchandradiwakar@gmail.com>
2024-01-04 00:51:35 +0530

    database added

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      "mongodb+srv://adityaagr694:adityaagr694@database.iy6osys.mongodb.net/learnFlow?retryWrites=true&w=majority",
+      "mongodb+srv://Wyomesh01:<Ratanraj>@users.dsgjfpz.mongodb.net/?retryWrites=true&w=majority",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
